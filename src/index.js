75f4dea52d4d64f1cb106d8a4c4580416c571fe7
oguz gül <ogzgl95@outlook.com>
2021-07-29 09:54:12 +0300

    import reducer and thunk tool on the index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducers/reducer";
+import { thunk } from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
---
+++ src/reducers/reducer.js
@@ -0,0 +1,8 @@
+const INITAL_STATE = {
+    countries: []
+};
+
+
+export const reducer = (state= INITAL_STATE, action) => {
+  return state;
+};
