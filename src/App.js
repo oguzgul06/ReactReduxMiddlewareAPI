c1f5caa9fa16a839bfdbed6e1a0c32d083c93c9e
oguz gül <ogzgl95@outlook.com>
2021-07-29 09:39:07 +0300

    create project first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Dersleri</h1>
+      <h2>React Router</h2>
+      {countries.map((country) => {
+        return (
+          <div key={country.name}>
+            <h3>{country.name}</h3>
+            <h4>{country.capital}</h4>
+            <p>
+              <img
+                src={country.flag}
+                alt={country.name}
+                style={{ width: "100px" }}
+              />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
